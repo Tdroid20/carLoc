b6db2317f0e0ff76ed7ddbddb2bd579b0ec5455b
Tdroid2.0 <garotorocha90@gmail.com>
2022-05-17 21:56:59 -0300

    🛠️┃Junção pt1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './global.css';
+import Perfil from './pages/JSX/auth/login';
+import Home from './pages/JSX/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+
+function RoutersApp() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate replace to="/home" />} />
+                <Route path='/home' element={ <Home /> }/>
+                <Route path='/profile' element={ <Perfil /> } />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+root.render(
+    <React.StrictMode>
+        <RoutersApp />
+    </React.StrictMode>
+);
---
+++ src/pages/JSX/home/index.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import { Link } from "react-router-dom";
+
+import '../../../assets/css/home.css';
+import Logo from '../../../assets/img/logoHome.png';
+import Banner from '../../../assets/img/banner.png';
+import CarroEconomico from '../../../assets/img/car1.png';
+import CarroEspecial from '../../../assets/img/car 2.png';
+import CarroLuxo from '../../../assets/img/car3.png';
+import Header from '../../../components/Header/header';
+import { Footer } from '../../../components/Footer/footer';
+
+const Home = () =>
+{
+    return (
+
+        <>
+
+            {/* Header */}
+
+            <Header />
+
+            {/* Main */}
+            <main>
+                <div className='alinhamentoHome'>
+                    <img className='logoHome' src={Logo} alt="" />
+
+                    <div>
+                        <img className='bannerHome' src={Banner} alt=""/>
+                    </div>
+
+                    <div className='btnHome'>
+                        <Link to='/profile' className="links"><button className='loginHome'>Login</button></Link>
+                        <Link to='/profile' className="links"><button className='saveHome'>Cadastrar</button></Link>
+                    </div>
+
+                </div>
+
+                <div className='cardHome'>
+                    <div className='economico'>
+                        <img className='carroEconomicoHome' src={CarroEconomico} alt="" />
+                        <p className='txtReservar'>Econômico</p>
+                        <button className='btnReservar'>Reservar</button>
+                    </div>
+
+                    <div className='especial'>
+                        <img className='carroEspecialHome' src={CarroEspecial} alt="" />
+                        <p className='txtReservar'>Especial</p>
+                        <button className='btnReservar'>Reservar</button>
+                    </div>
+
+                    <div className='luxo'>
+                        <img src={CarroLuxo} alt="" />
+                        <p className='txtReservar'>Luxo</p>
+                        <button className='btnReservar'>Reservar</button>
+                    </div>
+                </div>
+            </main>
+
+            {/* Footer */}
+
+            <Footer />
+
+        </>
+    )
+}
+
+export default Home;
